e92f080a4863528f50829c1a753e9a9ddce53e70
nrilu <11998876+nrilu@users.noreply.github.com>
2026-03-23 19:11:07 +0100

    dont auto-format tex files

--- init.lua
+++ init.lua
@@ -823,7 +823,8 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        --x Nicco: dont auto-format tex files!
+        local disable_filetypes = { c = true, cpp = true, tex = true }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
