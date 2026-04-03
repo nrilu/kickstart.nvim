9ca5c93b6e32c109ef6465afea1512ad5fea5778
nrilu <11998876+nrilu@users.noreply.github.com>
2026-04-03 03:12:59 +0200

    add multicursor

--- init.lua
+++ init.lua
@@ -1228,6 +1228,23 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'smoka7/multicursors.nvim',
+    event = 'VeryLazy',
+    dependencies = {
+      'nvimtools/hydra.nvim',
+    },
+    opts = {},
+    cmd = { 'MCstart', 'MCvisual', 'MCclear', 'MCpattern', 'MCvisualPattern', 'MCunderCursor' },
+    keys = {
+      {
+        mode = { 'v', 'n' },
+        '<Leader>m',
+        '<cmd>MCstart<cr>',
+        desc = 'Create a selection for selected text or word under the cursor',
+      },
+    },
+  },
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
   --  Here are some example plugins that I've included in the Kickstart repository.
