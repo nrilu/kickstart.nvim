14c2b5c8447612a0f7dbb389d8179fe70da1fe3d
nrilu <11998876+nrilu@users.noreply.github.com>
2026-08-05 17:57:46 +0200

    add blame setup options

--- init.lua
+++ init.lua
@@ -1432,7 +1432,32 @@ require('lazy').setup({
     'FabijanZulj/blame.nvim',
     lazy = false,
     config = function()
-      require('blame').setup {}
+      require('blame').setup {
+        date_format = '%d.%m.%Y',
+        virtual_style = 'right_align',
+        relative_date_if_recent = true, -- this is relative only for the latest month
+        views = {
+          window = window_view,
+          virtual = virtual_view,
+          default = window_view,
+        },
+        focus_blame = true,
+        merge_consecutive = false,
+        max_summary_width = 30,
+        colors = nil,
+        blame_options = nil,
+        commit_detail_view = 'vsplit',
+        -- format_fn = formats.commit_date_author_fn,
+        mappings = {
+          commit_info = 'i',
+          stack_push = '<TAB>',
+          stack_pop = '<BS>',
+          show_commit = '<CR>',
+          close = { '<esc>', 'q' },
+          copy_hash = 'y',
+          open_in_browser = 'o',
+        },
+      }
     end,
     opts = {
       blame_options = { '-w' },
